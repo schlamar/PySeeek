6044976b9485e0814adee5719eabe45d7d8d5c1d
ms4py <marc.schlaich@googlemail.com>
2010-12-05 00:13:44 +0100

    command line usage

--- seeek.py
+++ seeek.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
+import sys
+
 from pymongo import Connection
 
 from pyseeek.crawler import start_crawling
+
+def search(search_strings):
+    conn = Connection()
+    db = conn.pyseeek
+    print db.pages.find({'words.word': {'$in': search_strings}}).count()
 
 def clear_pages():
     conn = Connection()
     db = conn.pyseeek
     db.pages.remove()
+
+def crawl():
+    start_crawling(['http://web.de/', 'http://www.welt.de/',                    
+                    'http://www.bild.de/'])
+
         
 if __name__ == '__main__':
-    start_crawling(['http://web.de/', 'http://www.welt.de/',
-                    'http://www.bild.de/'])
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'crawl':
+            crawl()
+        elif sys.argv[1] == 'search':
+            search(sys.argv[2:])
+        elif sys.argv[1] == 'clear':
+            clear_pages()
                     
                     
 # db.pages.find({'words.word': 'test'}).count()
