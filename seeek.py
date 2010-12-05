62aafd44c8ca5a59fab0e9eb79577a20ba9479ed
ms4py <marc.schlaich@googlemail.com>
2010-12-05 11:59:19 +0100

    error corrected and usage improved

--- seeek.py
+++ seeek.py
@@ -14,14 +14,16 @@ def search(search_strings):
 def clear_pages():
     conn = Connection()
     db = conn.pyseeek
-    db.pages.drop()
+    db.drop_collection('pages')
 
 def crawl():
     start_crawling(['http://web.de/', 'http://www.welt.de/',                    
                     'http://www.bild.de/'])
 
 def set_index():
-    db.pages.ensureIndex({ "words.word": 1 });
+    conn = Connection()
+    db = conn.pyseeek
+    db.pages.ensure_index("words.word");
         
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -33,7 +35,9 @@ if __name__ == '__main__':
             clear_pages()
         elif sys.argv[1] == 'index':
             set_index()
-                    
+                   
+        else:
+            print 'Unknown argument "%s"' % sys.argv[1]
                     
 # db.pages.find({'words.word': 'test'}).count()
 # db.pages.find({"words.word": {$in: ["test", "hallo" , "heute"]}}).count()
